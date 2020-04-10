35ce899a99aa746fcacf9b86f953b32e67015d53
Florian Drinkler <flo.drinkler@gmail.com>
2020-04-10 18:29:00 +0200

    udpate indexes

--- backend/api/v1/index.js
+++ backend/api/v1/index.js
@@ -2,11 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.send("Hello v1.0 GET API");
+    res.send("Hello v1 GET API");
 });
 
 router.post("/", (req, res) => {
-    res.send("Hello v1.0 POST API");
+    res.send("Hello v1 POST API");
 });
 
 module.exports = router;
--- backend/api/v2/index.js
+++ backend/api/v2/index.js
@@ -2,11 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.send("Hello v2.0 GET API");
+    res.send("Hello v2 GET API");
 });
 
 router.post("/", (req, res) => {
-    res.send("Hello v2.0 POST API");
+    res.send("Hello v2 POST API");
 });
 
 module.exports = router;
