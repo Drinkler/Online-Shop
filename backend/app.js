8d8268347705acd76e92dc297201b20cacba6c85
Drinkler <flo.drinkler@gmail.com>
2020-04-10 15:38:00 +0200

    update db connection

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// TODO: api version
+
 // To read Request body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // Connect to db
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("Connected to database.");
 });
 
