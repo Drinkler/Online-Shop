9d1f0fa810dbbcbaeed374ce6e7685574a79d363
Drinkler <flo.drinkler@gmail.com>
2020-04-19 18:48:50 +0200

    added user login

--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -47,6 +47,30 @@ router.post("/signup", (req, res, next) => {
         });
 });
 
+router.post("/login", (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .exec()
+        .then((user) => {
+            if (user.length < 1) {
+                return res.status(401).json({ message: "Auth failed" });
+            }
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({ message: "Auth failed" });
+                }
+                if (result) {
+                    return res.status(200).json({ message: "Auth successful" });
+                }
+                return res.status(200).json({ message: "Auth failed" });
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err,
+            });
+        });
+});
+
 router.delete("/:userId", (req, res, next) => {
     User.deleteOne({ _id: req.params.userId })
         .exec()
