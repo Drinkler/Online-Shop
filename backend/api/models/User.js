77a2f0b8c80b268282ea0c4f830f8924923e5add
Drinkler <flo.drinkler@gmail.com>
2020-04-19 18:03:07 +0200

    added email regex

--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -7,6 +7,7 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     },
     password: {
         type: String,
