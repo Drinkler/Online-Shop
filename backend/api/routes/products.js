2e53639490fe7f88a3be28cb87d991f0033808c0
Drinkler <flo.drinkler@gmail.com>
2020-05-03 20:05:02 +0200

    added swagger

--- backend/api/routes/products.js
+++ backend/api/routes/products.js
@@ -5,6 +5,13 @@ const checkAuth = require("../middleware/check-auth");
 
 const ProductsController = require("../controllers/products");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: Managing Products
+ */
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./uploads/");
@@ -34,14 +41,78 @@ const upload = multer({
 // TODO : Check Auth
 // TODO : Upload Images
 
+/**
+ * @swagger
+ * path:
+ *  /products/:
+ *   get:
+ *    summary:
+ *    tags: [Products]
+ *    responses:
+ *     "200":
+ *      description: All Products
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/Product'
+ */
 router.get("/", ProductsController.getAllProducts);
 
+/**
+ * @swagger
+ * path:
+ *  /products/{productId}:
+ *   get:
+ *    summary: Get a product by id
+ *    tags: [Products]
+ *    parameters:
+ *     - in: path
+ *       name: productId
+ *       schema:
+ *        type: String
+ *       required: true
+ *       description: Id of a product
+ *    responses:
+ *     "200":
+ *      description: One Product
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/Product'
+ */
 router.get("/:productId", ProductsController.getProduct);
 
+/**
+ * @swagger
+ * path:
+ *  /products/:productId:
+ *   post:
+ *    summary:
+ *    tags: [Products]
+ *
+ */
 router.post("/", ProductsController.saveProduct);
 
+/**
+ * @swagger
+ * path:
+ *  /products/:
+ *   patch:
+ *    summary:
+ *    tags: [Products]
+ *
+ */
 router.patch("/:productId", ProductsController.updateProduct);
 
+/**
+ * @swagger
+ * path:
+ *  /products/:productId:
+ *   delete:
+ *    summary:
+ *    tags: [Products]
+ *
+ */
 router.delete("/:productId", ProductsController.deleteProduct);
 
 // * Old version with docs.map
