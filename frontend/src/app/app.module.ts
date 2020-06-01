2703d461dd2373cacfbd3945363481d7030d173c
Luca Stanger <lstanger@gmx.de>
2020-06-01 18:45:49 +0200

    ✨ Introducing new features

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,20 +3,31 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UiModule } from './ui/ui.module';
-import { LoginComponent } from "./ui/login/login.component";
+import { NavComponent } from './components/shared/nav/nav.component';
+import { HeaderComponent } from "./components/shared/header/header.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
-    UiModule
   ],
-  entryComponents: [LoginComponent],
+  entryComponents: [],
   providers: [],
   bootstrap: [AppComponent]
 })
