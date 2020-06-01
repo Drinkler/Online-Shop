087c5e1b0dde81fe17ed789842e96f6928665850
Luca Stanger <lstanger@gmx.de>
2020-06-01 19:58:34 +0200

    ✨ Introducing new features

---
+++ frontend/src/app/components/shopping-cart/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item',
+  templateUrl: './cart-item.component.html',
+  styleUrls: ['./cart-item.component.scss']
+})
+export class CartItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
