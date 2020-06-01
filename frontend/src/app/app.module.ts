45372d5ccea546f3d550352ff85fb64a96da7ed6
Luca Stanger <lstanger@gmx.de>
2020-06-01 20:00:48 +0200

    ♻ Refactored app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.
 import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
 import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
 import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { CartComponent } from './components/shopping-cart/cart/cart.component';
     ShoppingCartComponent,
     FiltersComponent,
     ProductListComponent,
-    CartComponent
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
