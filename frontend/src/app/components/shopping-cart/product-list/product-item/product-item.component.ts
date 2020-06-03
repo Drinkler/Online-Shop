2733b9a0c9f617055a02ac6c9b15667dd7311343
Luca Stanger <lstanger@gmx.de>
2020-06-03 11:58:15 +0200

    :art: add dynamic creation of product list

--- frontend/src/app/components/shopping-cart/product-list/product-item/product-item.component.ts
+++ frontend/src/app/components/shopping-cart/product-list/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from "src/app/models/product";
 
 @Component({
   selector: 'app-product-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.scss']
 })
 export class ProductItemComponent implements OnInit {
+
+  @Input() productItem: Product;
 
   constructor() { }
 
--- frontend/src/app/components/shopping-cart/product-list/product-list.component.ts
+++ frontend/src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from "src/app/services/product.service";
+import { Product } from "../../../models/product";
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  productList: Product[] = [];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productList = this.productService.getProducts()
   }
 
 }
