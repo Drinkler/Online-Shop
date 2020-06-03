e1595a3f64639b7ba4a4bcf20c27651a29c94651
Luca Stanger <lstanger@gmx.de>
2020-06-03 11:56:36 +0200

    :art: add dynamic shopping cart visibility

--- frontend/src/app/components/shopping-cart/cart/cart.component.ts
+++ frontend/src/app/components/shopping-cart/cart/cart.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  cartItems = [
+    {id: 1, productId: 1, productName: 'Item1', qty: 4, price: 100},
+    {id: 2, productId: 3, productName: 'Item2', qty: 3, price: 22},
+    {id: 3, productId: 2, productName: 'Item3', qty: 2, price: 25},
+    {id: 4, productId: 4, productName: 'Item4', qty: 5, price: 257},
+  ];
+
+  cartTotal = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.cartItems.forEach(item => {
+      this.cartTotal += (item.qty * item.price)
+    })
   }
 
 }
