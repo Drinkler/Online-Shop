11b38f4fbb23a209ead1b1b0f34b0ef7a74ac2dd
Luca Stanger <lstanger@gmx.de>
2020-06-03 11:11:42 +0200

    :package: NEW: add product model

---
+++ frontend/src/app/models/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+  id: number;
+  name: String;
+  description: String;
+  price: number;
+  imageUrl: string;
+
+  constructor(id: number, name: String, description: String = '', price: number = 0, imageUrl: string = 'https://ih1.redbubble.net/image.449121551.2876/st,small,507x507-pad,600x600,f8f8f8.u3.jpg') {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.imageUrl = imageUrl;
+  }
+}
