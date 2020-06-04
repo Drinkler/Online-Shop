12303bfeb4ba28a26d03e7268646369c6e1e6238
Luca Stanger <lstanger@gmx.de>
2020-06-04 17:46:59 +0200

    :sparkles: FEAT: add product creation via backend communication

--- frontend/src/app/components/shopping-cart/product-list/product-list.component.ts
+++ frontend/src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -14,7 +14,8 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productList = this.productService.getProducts()
+    this.productService.getProducts().subscribe((products) => {
+      this.productList = products["products"];
+    })
   }
-
 }
