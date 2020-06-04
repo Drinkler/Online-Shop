9a872a66b8cf8aef36120189a7d8aa8758a37fbc
Luca Stanger <lstanger@gmx.de>
2020-06-04 17:44:51 +0200

    :arrow_up: FEAT: add HttpClientModule dependecy

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,7 +11,8 @@ import { FiltersComponent } from './components/shopping-cart/filters/filters.com
 import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
 import { CartComponent } from './components/shopping-cart/cart/cart.component';
 import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
-import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
+import {ProductItemComponent} from './components/shopping-cart/product-list/product-item/product-item.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { ProductItemComponent } from './components/shopping-cart/product-list/pr
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
+    HttpClientModule
   ],
   entryComponents: [],
   providers: [],
