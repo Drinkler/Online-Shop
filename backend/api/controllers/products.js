2f666eac5cde32754e32edd9190031df86c1a546
Drinkler <flo.drinkler@gmail.com>
2020-06-04 21:58:19 +0200

    :sparkles: FEAT: products are now available

--- backend/api/controllers/products.js
+++ backend/api/controllers/products.js
@@ -1,104 +1,115 @@
 const Product = require("../models/Product");
 
-exports.saveProduct = (req, res, next) => {
+exports.saveProduct = async (req, res, next) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    // Create a new Product
     const product = new Product({
-        name: req.body.name,
-        price: req.body.price,
-        description: req.body.description,
+        name: name,
+        price: price,
+        description: description,
     });
 
-    product
-        .save()
-        .then((product) => {
-            console.log("save");
-            res.status(200).json({
-                message: "Product saved successfully.",
-                ok: 1,
-                createdProduct: {
-                    _id: product._id,
-                    name: product.name,
-                    price: product.price,
-                    created: product.created,
-                    request: {
-                        type: "GET",
-                        url: req.protocol + "://" + req.get("host") + req.originalUrl + product._id,
-                    },
-                },
-            });
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
+    // Save product
+    try {
+        const savedProduct = await product.save();
+        return res.status(200).json({
+            message: "Product successfully created.",
+            createdProduct: {
+                _id: savedProduct._id,
+                name: savedProduct.name,
+                price: savedProduct.price,
+                description: savedProduct.description,
+            },
         });
+    } catch (err) {
+        return res.status(500).json({ error: err });
+    }
 };
 
-exports.getAllProducts = (req, res, next) => {
-    Product.find()
-        .select("-__v -created")
-        .exec()
-        .then((docs) => {
-            res.status(200).json({
-                amount: docs.length,
-                products: docs,
-            });
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
-        });
-};
-
-exports.getProduct = (req, res, next) => {
-    Product.findOne({ _id: req.params.productId })
-        .select("-__v -created")
-        .exec()
-        .then((product) => {
-            res.status(200).json(product);
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
-        });
-};
-
-exports.deleteProduct = (req, res, next) => {
-    Product.deleteOne({ _id: req.params.productId })
-        .exec()
-        .then((result) => {
-            if (result.n == 0 || result.deletedCount == 0) {
-                res.status(202).json({
-                    message: "Product not found or already deleted.",
-                    result: result,
-                });
-            } else {
-                res.status(200).json({
-                    message: "Product deleted.",
-                    result: result,
-                });
-            }
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
-        });
-};
-
-exports.updateProduct = (req, res, next) => {
-    /**
-     * Doesn't need all parameters of a product.
-     * Only parameters who are defined in the model can be changed.
-     * Parameters who aren't defined in the model are getting ignored.
-     */
-    const updateOperators = {};
-    for (const operator of req.body) {
-        updateOperators[operator.propName] = operator.value;
+exports.getAllProducts = async (req, res, next) => {
+    // Get all products
+    try {
+        var products = await Product.find().select("-__v").exec();
+    } catch (err) {
+        return res.status(500).json({ error: err });
     }
 
-    Product.updateOne({ _id: req.params.productId }, { $set: updateOperators })
-        .exec()
-        .then((result) => {
-            res.status(200).json({
-                message: "Product updated.",
-                result: result,
+    // Return all products
+    return res.status(200).json({
+        amount: products.length,
+        products: products,
+    });
+};
+
+exports.getProduct = async (req, res, next) => {
+    const productId = req.params.productId;
+
+    // Get product by productId
+    try {
+        var product = await Product.findOne({ _id: productId }).select("-__v").exec();
+    } catch (err) {
+        return res.status(500).json({ error: "No Product found or Internal Error." });
+    }
+
+    // Return product data
+    return res.status(200).json(product);
+};
+
+exports.deleteProduct = async (req, res, next) => {
+    const productId = req.params.productId;
+
+    // Delete product by productId
+    try {
+        const result = await Product.deleteOne({ _id: productId }).exec();
+        if (result.n != 0 && result.deletedCount != 0) {
+            return res.status(200).json({
+                message: "Product successfully deleted.",
+                ok: result.ok,
             });
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
-        });
+        }
+    } catch (err) {
+        return res.status(500).json({ error: err });
+    }
+
+    // Return no product
+    return res.status(409).json({
+        message: "No Product was found to delete.",
+        ok: 0,
+    });
+};
+
+exports.updateProduct = async (req, res, next) => {
+    const productId = req.params.productId;
+
+    const parameters = {};
+    const allowedKeys = ["name", "price", "description"];
+
+    // Get parameters of body
+    Object.assign(parameters, req.body);
+
+    // Get keys
+    const keys = Object.keys(parameters);
+
+    // Remove keys which are not valid
+    keys.forEach((key) => {
+        if (!allowedKeys.includes(key)) delete parameters[key];
+    });
+
+    // Update product information
+    try {
+        const result = await Product.updateOne({ _id: productId }, { $set: parameters }).exec();
+        if (result.n != 0 && result.nModified != 0) {
+            return res.status(200).json({
+                message: "Product successfully updated.",
+                ok: result.ok,
+            });
+        }
+    } catch (err) {
+        return res.status(500).json({ error: err });
+    }
+
+    return res.status(400).json({ error: "Product not found, or couldn't update product." });
 };
