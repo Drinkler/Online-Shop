65a2d371e1f2f808696a7233426ef95344db1bd4
Florian Drinkler <flo.drinkler@gmail.com>
2020-06-07 20:31:44 +0200

    :art: REFACTOR: add timestamp to review schema

--- backend/api/models/Review.js
+++ backend/api/models/Review.js
@@ -1,31 +1,32 @@
 const mongoose = require("mongoose");
 
-const reviewSchema = mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        require: true,
+const reviewSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            require: true,
+        },
+        title: {
+            type: String,
+            require: true,
+            maxlength: 50,
+        },
+        message: {
+            type: String,
+            require: true,
+            maxlength: 300,
+        },
+        rating: {
+            type: Number,
+            require: true,
+            min: 0,
+            max: 10,
+        },
     },
-    created: {
-        type: Date,
-        default: Date.now(),
-    },
-    title: {
-        type: String,
-        require: true,
-        maxlength: 50,
-    },
-    message: {
-        type: String,
-        require: true,
-        maxlength: 300,
-    },
-    rating: {
-        type: Number,
-        require: true,
-        min: 0,
-        max: 10,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = mongoose.model("Review", reviewSchema);
