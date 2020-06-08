3cf3393fcabb5d217588d4bd4826d9c3aeca6dbc
Luca Stanger <lstanger@gmx.de>
2020-06-08 18:45:07 +0200

    :package: NEW: add account module

---
+++ frontend/src/app/account/account.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+
+import {AccountRoutingModule} from './account-routing.module';
+import {LayoutComponent} from './layout.component';
+import {LoginComponent} from "../components/login/login.component";
+import {RegisterComponent} from "../components/register/register.component";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AccountRoutingModule
+  ],
+  declarations: [
+    LayoutComponent,
+    LoginComponent,
+    RegisterComponent
+  ]
+})
+export class AccountModule {
+}
