6ab4bcf4fc73a496b12f91a6d74b8b45e147ebca
Luca Stanger <lstanger@gmx.de>
2020-06-09 09:17:24 +0200

    :ambulance: FIX: critical hotfix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  title = 'Online-Shop';
+
   constructor() {
   }
 
--- frontend/src/app/config/api.ts
+++ frontend/src/app/config/api.ts
@@ -1,4 +1,4 @@
-import {environment} from "src/environments/environment";
+import {environment} from 'src/environments/environment';
 
 export const baseUrl = environment.production ? 'http://' : 'http://localhost:8080';
 export const productsUrl = baseUrl + '/rest/api/products';
