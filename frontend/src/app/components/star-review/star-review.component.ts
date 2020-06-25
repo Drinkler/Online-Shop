5d0012039e54985bfa7a9b04a206aabe5efc8fed
lucastanger <lstanger@gmx.de>
2020-06-25 10:00:32 +0200

    :poop: FEAT: add star voting system

---
+++ frontend/src/app/components/star-review/star-review.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {StarService} from 'src/app/services/star.service';
+
+@Component({
+  selector: 'app-star-review',
+  templateUrl: './star-review.component.html',
+  styleUrls: ['./star-review.component.scss']
+})
+export class StarReviewComponent implements OnInit {
+
+  @Input() userId;
+  @Input() productId;
+
+  stars: Observable<any>;
+
+  constructor(
+    private starService: StarService
+  ) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  starHandler(value) {
+    this.starService.setStar(this.userId, this.productId, value);
+  }
+
+}
---
+++ frontend/src/app/services/star.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+
+export interface Star {
+  userId: any;
+  productId: any;
+  value: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StarService {
+
+  constructor() {
+  }
+
+  setStar(userId: any, productId: any, value: any) {
+
+  }
+}
